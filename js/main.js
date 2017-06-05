7a2b8e3090c9ac6eca3fd5616e88c5f1875e65ce
eighteencharacters <feldman.b@protonmail.com>
2017-06-05 10:52:55 +0200

    C

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    $("body").append("<p>" + Math.round(Math.random() * 10 * Math.random() * 100) + "</p>")
+    $("body").append("<p>" + Math.round(Math.random() * 10 * Math.random() * 100) + "</p>");
+    $("button").click(function(){
+        $("p").html(Math.round(Math.random() * 10 * Math.random() * 100));
+    });
 });
 
 function doSomething() {
